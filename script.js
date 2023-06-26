f577f9ef3250e51c9ea97c5799dcb972d467df3f
UmanchukMargarita <133900836+UmanchukMargarita@users.noreply.github.com>
2023-06-26 18:23:50 +0300

    hello<3cute

---
+++ script.js
@@ -0,0 +1,29 @@
+let name = "Марго";
+let nameLength = name[0];
+//alert("Добрий день" +nameLength);
+
+
+name = "Марго";
+let age = 14;
+let empty;
+empty = (name + age);
+alert(name + age + empty);
+nameLength = empty.length;
+let q = name[2];
+console.log(q);
+
+let fruits =["Яблуко","Апельси","Слива"];
+alert( fruits[0] ); // Яблуко 
+alert( fruits[1] ); // Апельсин
+alert( fruits[2] ); // Слива 
+
+ fruits[3]= "Груша";
+ alert(fruits.pop());
+ alert(fruits);
+
+ let fruits = ["Апельсин" , "Груша"];
+
+ let people =[ "Дима" , "Марго" ];
+ people[3]= " Останній";
+people[2]= "Заміна";
+alert(peoole.length);
